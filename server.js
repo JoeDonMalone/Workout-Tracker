227c4e81b0d0f1e723a03095c6ed3818533e28a9
Joe Malone <joe.d.malone@gmail.com>
2021-05-01 14:12:47 -0500

    fixed mongo name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
